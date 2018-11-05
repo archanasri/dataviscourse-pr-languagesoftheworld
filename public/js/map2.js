83d7ca59391fd018b5b3919e65aa84230abdc504
Krithika Iyer <iyerkrithika21@gmail.com>
2018-11-05 01:00:03 -0700

    Added map

--- public/js/map2.js
+++ public/js/map2.js
@@ -47,98 +47,44 @@ class Map {
         //note that projection is global!
         console.log(this.languageData);
         console.log(this.nameArray);
+        console.log(world);
         let geojson = topojson.feature(world, world.objects.countries);
 
-        let countryData = geojson.features.map(country => {
-
-            let index = this.nameArray.indexOf(country.id);
-            let region = 'countries';
-
-            if (index > -1) {
-                //  console.log(this.populationData[index].geo, country.id);
-                //region = this.populationData[index].region;
-                //return new CountryData(country.type, country.id, country.properties, country.geometry, region);
-            } else {
-                // console.log('not found');
-                //return new CountryData(country.type, country.id, country.properties, country.geometry, 'countries');
-            }
-
-        });
-
-        // console.log(countryData);
-        let path = d3.geoPath().projection(this.projection);
-        let world_map = d3.select("#map-chart");
-        let svg = world_map.append("svg");
-
+        let p = d3.geoPath().projection(this.projection);
+        let world_map = d3.select("#map").select("svg")
+        let svg = world_map;
+//.append("svg");
         let world_map_paths = svg.selectAll("path")
-           //.data(countryData)
-           .enter()
+           .data(geojson.features);
+          world_map_paths.enter()
            .append("path")
-           .attr("d",path)
-           .attr("class",function(d){return d.region})
-           .attr("id",function(d){return d.id});
+           .attr("fill","grey")
+           .attr("stroke","black")
+           .attr("d",p);
 
         let graticule = d3.geoGraticule();
        	svg.append("path")
        		 .datum(graticule)
-           .attr("class", "graticule")
-           .attr("d", path);
+           .attr("d", p)
+           .attr("stroke","red")
+           .attr("fill","none");
 
 
        	svg.append("path")
             .datum(graticule.outline)
-            .attr("d", path)
-       		  .attr("class","stroke");
-
-       let that = this;
-       world_map_paths.on("click", function(d){
-           console.log(d.id);
-           return that.updateCountry(d.id);
-           });
+            .attr("fill","none")
+            .attr("d", p)
+            .attr("stroke","black");
 
 
-    }
 
-    /**
-     * Highlights the selected conutry and region on mouse click
-     * @param activeCountry the country ID of the country to be rendered as selected/highlighted
-     */
-    updateHighlightClick(activeCountry) {
-        // ******* TODO: PART 3*******
-        // Assign selected class to the target country and corresponding region
-        // Hint: If you followed our suggestion of using classes to style
-        // the colors and markers for countries/regions, you can use
-        // d3 selection and .classed to set these classes on here.
-        //
-        //TODO - Your code goes here -
-        this.clearHighlight();
-
-        let pathRegionSelection = undefined;
-        pathRegionSelection= d3.select("#"+activeCountry);
-        pathRegionSelection.classed("selected-country",true);
 
 
 
 
     }
 
-    /**
-     * Clears all highlights
-     */
-    clearHighlight() {
-        // ******* TODO: PART 3*******
-        // Clear the map of any colors/markers; You can do this with inline styling or by
-        // defining a class style in styles.css
-
-        // Hint: If you followed our suggestion of using classes to style
-        // the colors and markers for hosts/teams/winners, you can use
-        // d3 selection and .classed to set these classes off here.
-
-        //TODO - Your code goes here -
-
-        let map = d3.select('#map-chart').selectAll("path").classed('selected-country',false);
-        console.log("test");
 
 
-    }
+
 }
